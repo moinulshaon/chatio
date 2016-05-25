ab0e7768e29e3e46a0c91d88bf6d7548b3425ce6
moinul.shaon <moinul.shaon@gmail.com>
2016-05-24 22:49:03 -0700

    username and private message added

--- app.js
+++ app.js
@@ -11,12 +11,51 @@ app.get('/',function(req,res){
 });
  
 
+
+var allUsers=[];
+var rooms=[];
+
 io.sockets.on(  'connection',function( socket ){
 
 	//send message
 	socket.on('send message',function(data){
-		io.sockets.emit( 'new message',{msg:data} );
+		if ( data.receivername == undefined || data.receivername == "" ){
+			io.sockets.emit( 'new message',{
+				sendername:socket.myname,
+				msg:data.msg
+			});	
+		}else{
+			socket.to( rooms[ data.receivername ] ).emit( 'new message',{
+				sendername:socket.myname,
+				msg:data.msg
+			});
+			socket.emit( 'new message',{
+				sendername:socket.myname,
+				msg:data.msg
+			});
+		}
 	});
-
-
+	
+	//new user comes
+	socket.on('new user',function(data){
+		io.sockets.emit('new user',{username:data});
+		
+		socket.myname = data;
+		rooms[data] = socket.id;
+		
+		allUsers.push(data);
+		socket.emit( 'fetched users' , allUsers );
+		
+	});
+	
+	
+	socket.onclose = function(reason){
+    	delete rooms[this.myname];
+		var foundIndex = allUsers.indexOf(this.myname);
+		if ( foundIndex >=0 )
+    		allUsers.splice(  foundIndex , 1 );
+		io.emit( 'fetched users' , allUsers );
+		Object.getPrototypeOf(this).onclose.call(this,reason);
+	};
+	
 } );
